bfb8d797c006e57d94105926c3bbd0532bdd7c8a
Michael Wild <michael.wild@andritz.com>
2019-06-18 10:24:58 +0200

    Adds a function to mimic the AUTOMOC

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,4 +1,5 @@
 #include "mainwindow.h"
+#include "utils.h"
 
 MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent)
---
+++ utils.h
@@ -0,0 +1,7 @@
+#pragma once
+#ifndef UTILS_H
+#define UTILS_H
+
+void foo();
+
+#endif // UTILS_H
